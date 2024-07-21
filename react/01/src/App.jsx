72a1649c51c59c4806c6f8f8865c85b54fd0aafb
Imtiyaz Baig Moghal <ImtiyazBaig.Moghal@samsclub.com>
2024-07-21 16:32:55 -0500

    mods

--- react/01/src/App.jsx
+++ react/01/src/App.jsx
@@ -19,13 +19,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            {
-                CORE_CONCEPTS.map((conceptItem) => 
-                    <CoreConcept {...conceptItem} />
-            )
-            }
-            
-            
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
